085e644195f6f8d7141568633bb109bbc3116bc8
valag22 <valag2206@gmail.com>
2026-07-27 23:35:17 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ from telebot.types import (
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8921489424:AAFCrTyaD6S-Zd2sFav_7-WBH9KQDfB7Cmk")
 
-PANEL_BASE = os.environ.get("PANEL_BASE", "https://odd-hill-595b.valasharif92.workers.dev/")
+PANEL_BASE = os.environ.get("PANEL_BASE", "https://odd-hill-595b.valasharif92.workers.dev")
 PANEL_API_ROUTE = os.environ.get("PANEL_API_ROUTE", "sync")
 
 PANEL_API_KEY = os.environ.get("PANEL_API_KEY", "nahan_ms3nempd_itecble0")
