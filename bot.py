969e556c865672db36636c31df6cb7520c375c37
valag22 <valag2206@gmail.com>
2026-07-27 23:38:17 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,11 +20,11 @@ from telebot.types import (
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8921489424:AAFCrTyaD6S-Zd2sFav_7-WBH9KQDfB7Cmk")
 
-PANEL_BASE = os.environ.get("PANEL_BASE", "https://odd-hill-595b.valasharif92.workers.dev")
+PANEL_BASE = os.environ.get("PANEL_BASE", "https://little-waterfall-27fa.berbrtokamma.workers.dev")
 PANEL_API_ROUTE = os.environ.get("PANEL_API_ROUTE", "sync")
 
-PANEL_API_KEY = os.environ.get("PANEL_API_KEY", "nahan_ms3nempd_itecble0")
-PANEL_MASTER_KEY_FALLBACK = os.environ.get("PANEL_MASTER_KEY", "vala92")
+PANEL_API_KEY = os.environ.get("PANEL_API_KEY", "nahan_mrlmsp7c_7lg9rlf0")
+PANEL_MASTER_KEY_FALLBACK = os.environ.get("PANEL_MASTER_KEY", "vala1392")
 
 PANEL_AUTH_HEADERS = {"Authorization": f"Bearer {PANEL_API_KEY}"}
 
